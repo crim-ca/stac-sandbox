fefa1d0cf0a284a8d6d30ddd47ac73ce8f9d4472
Mathieu Provencher <>
2022-04-20 19:37:10 -0400

    added ceda ui

--- app/stac_app.py
+++ app/stac_app.py
@@ -50,7 +50,13 @@ api = StacApi(
 )
 app = api.app
 
-app.add_middleware(CORSMiddleware, allow_origins=["*"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
