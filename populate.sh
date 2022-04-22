6230335336ec03ae09151b8e66eeccce396646bc
Mathieu Provencher <>
2022-04-21 23:12:55 -0400

    ignore

--- populate.sh
+++ populate.sh
@@ -4,6 +4,8 @@ set -ex
 # requires pip installs done
 # requires running STAC server
 
+# reset the STAC API data by using `docker-compose down -v; docker-compose up -d --force-recreate`
+
 cd ../asset-scanner-example
 eval "$(conda shell.bash hook)"
 conda activate ceda-workflow-3.9-1
@@ -12,8 +14,8 @@ conda activate ceda-workflow-3.9-1
 python3 scripts/collection_processor.py scripts/collections.yaml
 
 # add items
-python3 -m asset_scanner.scripts.asset_scanner conf/thredds-extract-assets.yaml        # CMIP5
-python3 -m asset_scanner.scripts.asset_scanner conf/thredds-extract-assets-2.yaml      # CMIP6
+python3 -m asset_scanner.scripts.asset_scanner conf/thredds-extract-assets-2.yaml        # CMIP6
+python3 -m asset_scanner.scripts.asset_scanner conf/thredds-extract-assets.yaml          # CMIP5
 
 # update collection summaries
 python3 scripts/collection_processor.py scripts/collections.yaml
