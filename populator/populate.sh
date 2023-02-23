b848e37d6a9a6bdff4504506b14013758398eb60
Mathieu Provencher <>
2023-02-22 22:16:02 -0500

    added timeout

--- populator/populate.sh
+++ populator/populate.sh
@@ -6,6 +6,8 @@ set -ex
 
 # reset the STAC API data by using `docker-compose down -v; docker-compose up -d --force-recreate`
 
+ASSET_GENERATOR_TIMEOUT=20
+
 eval "$(conda shell.bash hook)"
 conda activate ceda-workflow-3.9-1
 
@@ -15,8 +17,17 @@ python3 collection_processor.py collections.yaml
 cd ~/projects/stac-generator-example
 
 # add items
-python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip6-asset-generator.yaml        # CMIP6
-python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip5-asset-generator.yaml        # CMIP5
+timeout -s HUP $ASSET_GENERATOR_TIMEOUT bash -c "python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip6-asset-generator.yaml" &
+PID_CMIP6=$!
+timeout -s HUP $ASSET_GENERATOR_TIMEOUT bash -c "python3 -m stac_generator.scripts.stac_generator conf/thredds-cmip5-asset-generator.yaml" &
+PID_CMIP5=$!
+
+# here we kill the asset generators if CTRL-C is sent
+trap "kill -2 ${PID_CMIP6} ${PID_CMIP5} 2> /dev/null; exit 1" INT
+
+echo "Running STAC asset generator for $ASSET_GENERATOR_TIMEOUT seconds..."
+wait $PID_CMIP6
+wait $PID_CMIP5
 
 cd ~/projects/stac-sandbox/populator
 
